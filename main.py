922400cdc6888f85d2b805ae97559e0d25e88471
Mads Lundwall <mads@lundwall.dk>
2022-12-20 18:59:49 +0100

    Kommentarer tilføjet

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-# This is a sample Python script.
-import tkinter
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Test interaktiv grafik med Tkinter
+# Mads Lundwall 2022
+
 from tkinter import *
 
 
 def drawline(event):
-    print(event.type)
+    # Vi har modtaget et musetryk
+    # print(event.type)
     if str(event.type) == '4':  # 'ButtonPress':
+        # Når vi trykker husker vi positonen
         canvas.old_coords = event.x, event.y
-
     elif str(event.type) == '5':  #'ButtonRelease':
+        # Når vi løfter, tegner vi linjen
         x, y = event.x, event.y
         x1, y1 = canvas.old_coords
         canvas.create_line(x, y, x1, y1)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Vi kører det som "hovedprogram", dvs eksekverer dette skript - ie. vi importerer det ikke
+    # Opret et tkinter-rodvindue
     root = Tk()
 
+    # Vi sætter hovedvinduets størrelse
+    root.geometry("700x350")
+
+    # Vi laver en overskrift
     label = Label(root, text="Grafik")
     label.pack()
 
-    # Set the size of the tkinter window
-    root.geometry("700x350")
-
-    # Create a canvas widget
+    # Vi laver et område at tegne på
     canvas = Canvas(root, width=500, height=300)
     canvas.pack()
 
-    # Add a line in canvas widget
-    canvas.create_line(100, 200, 200, 35, fill="black", width=1)
+    # Vi binder to events til musetryk/løft:
     root.bind('<ButtonPress-1>', drawline)
     root.bind('<ButtonRelease-1>', drawline)
 
+    # Hoved - eventloop:
     root.mainloop()
