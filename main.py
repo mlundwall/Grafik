e658f20684663e3d153a250c1234c6599b8d14af
Mads Lundwall <mjln@tec.dk>
2022-12-21 12:56:04 +0100

    Tilretninger, knapper og file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Test interaktiv grafik med Tkinter
 # Mads Lundwall 2022
 from tkinter import *
+import sys
 
 
 def drawline(event):
@@ -9,11 +10,22 @@ def drawline(event):
     if str(event.type) == '4':  # 'ButtonPress':
         # Når vi trykker husker vi positonen
         canvas.old_coords = event.x, event.y
-    elif str(event.type) == '5':  #'ButtonRelease':
+    elif str(event.type) == '5':  # 'ButtonRelease':
         # Når vi løfter, tegner vi linjen
         x, y = event.x, event.y
         x1, y1 = canvas.old_coords
         canvas.create_line(x, y, x1, y1)
+
+
+def knaptryk():
+    label.config(text="Grafik - nu med tryk")
+    original_stdout = sys.stdout        # SReference til standard output
+
+    with open('tekstfil.txt', 'w') as f:
+        sys.stdout = f                  # Flyt stdout til filen.
+        print(label.cget("text") +
+              " og output til fil")     # cget == hent config-variabel, her text
+        sys.stdout = original_stdout    # Reset the standard output to its original value
 
 
 if __name__ == '__main__':
@@ -22,15 +34,18 @@ if __name__ == '__main__':
     root = Tk()
 
     # Vi sætter hovedvinduets størrelse
-    root.geometry("700x350")
+    root.geometry("700x550")
 
     # Vi laver en overskrift
     label = Label(root, text="Grafik")
     label.pack()
 
     # Vi laver et område at tegne på
-    canvas = Canvas(root, width=500, height=300)
+    canvas = Canvas(root, width=600, height=450)
     canvas.pack()
+
+    knap = Button(root, command=knaptryk, text="    Knap    ")
+    knap.pack()
 
     # Vi binder to events til musetryk/løft:
     root.bind('<ButtonPress-1>', drawline)
